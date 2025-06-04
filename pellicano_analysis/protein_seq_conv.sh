afc1b152f846aeb65caff5188ea56eefc7425e6b
ielisep <149195502+ielisep@users.noreply.github.com>
2025-06-04 11:24:47 -0400

    fixed framebot build

--- pellicano_analysis/protein_seq_conv.sh
+++ pellicano_analysis/protein_seq_conv.sh
@@ -2,13 +2,15 @@
 module purge
 module load Miniforge3
 conda activate framebot
-ant -f Framebot/build.xml jar
-cd /mnt/home/f0111521/RDPTools/
-Framebot framebot
 
 merged_fastq_dir=/mnt/research/EvansLab/Isabella/merged_fastq
 nifH_prot_ref=/mnt/research/EvansLab/Isabella/nifH_tools/nifh_prot_ref.fasta
+framebot=/mnt/home/f0111521/RDPTools/
+
+cd $framebot
+ant -f Framebot/build.xml jar
+
 
 # Translate OTU representative sequences into protein sequences
 cd $merged_fastq_dir
-framebot -N -l 30 -i 0.4 -o $merged_fastq_dir $nifH_prot_ref $merged_fastq_dir/rep_seqs.fa
+$framebot FrameBot framebot -N -l 30 -i 0.4 -o $merged_fastq_dir $nifH_prot_ref $merged_fastq_dir/rep_seqs.fa
