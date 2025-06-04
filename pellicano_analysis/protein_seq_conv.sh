92e7ad7d9e2c69821f245d9c100bd94f3ac784d3
ielisep <149195502+ielisep@users.noreply.github.com>
2025-06-04 10:21:28 -0400

    Adding protein sequence conversion files

---
+++ pellicano_analysis/protein_seq_conv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+module purge
+module load Miniforge3
+conda activate framebot
+
+merged_fastq_dir=/mnt/research/EvansLab/Isabella/merged_fastq
+nifH_prot_ref=/mnt/research/EvansLab/Isabella/nifH_tools/nifh_prot_ref.fasta
+
+# Translate OTU representative sequences into protein sequences
+cd $merged_fastq_dir
+framebot -N -l 30 -i 0.4 -o $merged_fastq_dir $nifH_prot_ref $merged_fastq_dir/rep_seqs.fa
